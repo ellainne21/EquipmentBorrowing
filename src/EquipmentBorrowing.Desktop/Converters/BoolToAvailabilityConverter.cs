55446a4086d6e23edea28c53ac52c7c4b55c626e
Ella <ellainnecabasag9@gmail.com>
2026-09-04 14:26:26 +0800

    Add EquipmentView with data binding, borrow form, and availability converter

---
+++ src/EquipmentBorrowing.Desktop/Converters/BoolToAvailabilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace EquipmentBorrowing.Desktop.Converters;
+
+public class BoolToAvailabilityConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool isAvailable)
+        {
+            return isAvailable ? "Available" : "Unavailable";
+        }
+        return "Unknown";
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
